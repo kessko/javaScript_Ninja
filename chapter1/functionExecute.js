87226d997cf0ffa84962f8da24ec84b4e77ecf0a
sgolub <artskf@gmail.com>
2015-11-23 00:17:05 +0300

    read some pages. write some code

---
+++ chapter1/functionExecute.js
@@ -0,0 +1,93 @@
+/**
+ * Created by Work on 22-Nov-15.
+ */
+window.onload = function () {
+    var obj = {};
+    var fn = function () {
+
+    };
+    assert(obj && fn, 'we have function and object');
+
+    fn.prop = 'function property';
+    obj.prop = 'object property';
+
+    assert(fn.prop, 'we can add property to the function. like this ' + fn.prop);
+
+    var storage = {
+        functionIndex: 1,
+        add: function (fn) {
+            if (!fn.id) {
+                fn.id = storage.functionIndex++;
+
+                return !!(   storage.cache[fn.id] = fn);
+            }
+
+        },
+        cache: []
+    };
+    var emptyFn = function () {
+    };
+
+    assert(storage.add(emptyFn), 'was added');
+    assert(!storage.add(emptyFn), "wasn't added again");
+
+
+    function isPrime(value) {
+        if (!isPrime.answer) {
+            isPrime.answer = {}
+        }
+        if (isPrime.answer[value] != null) {
+            return isPrime.answer[value];
+        }
+        var prime = value != 1;
+        for (var i = 2; i < value / i; i++) {
+            if (value % i === 0) {
+                prime = false;
+                break;
+            }
+        }
+        return isPrime.answer[value] = prime;
+    }
+    assert(isPrime(5), '5 is prime number');
+    assert(isPrime.answer[5], 'now we use cached value');
+
+
+    function getElement(name) {
+        if (!getElement.cache) {
+            getElement.cache = {};
+        }
+        return getElement.cache[name] = getElement.cache[name] || document.getElementsByName(name);
+
+    }
+
+    assert(getElement('body'), 'we get body');
+    assert(getElement.cache['body'], 'and get this from cache!');
+
+    var elems = {
+        length: 0,
+        add: function (elem) {
+            Array.prototype.push.call(this, elem);
+        },
+        gather: function (id) {
+            this.add(document.getElementById(id));
+        }
+
+    };
+    elems.gather('first');
+    assert(elems.length === 1 && elems[0].nodeType, 'we have 1 element in collection');
+    elems.gather('second');
+    assert(elems.length === 2 && elems[1].nodeType, 'now we have 2 element. yeap!');
+
+
+    var smallest = function (array) {
+        return Math.min.apply(Math, array);
+    };
+    var largest = function (array) {
+        return Math.max.apply(Math, array);
+    };
+
+    assert(smallest([1,2,3,4,5]) === 1, '1 was the smallest number oin array');
+    assert(largest([99,44,1,8]) === 99, '99 is the biggest number in the array');
+
+
+};
